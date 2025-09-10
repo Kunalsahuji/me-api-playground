68762486abb0644787c7fb23e3933c12a1b82a9f
Kunalsahuji <ksahu0103@gmail.com>
2025-09-11 00:03:51 +0530

    Add rate limiting middleware to control request frequency

---
+++ src/middleware/rateLimit.js
@@ -0,0 +1,7 @@
+const rateLimit = require('express-rate-limit');
+module.exports = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 200, // limit each IP to 200 requests per windowMs
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
